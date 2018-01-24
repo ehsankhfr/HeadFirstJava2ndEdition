34aa140fe74a02b9275020cc76fec9dc96bf7a36
Ehsan <ehsan@Ehsans-Air.home>
2018-01-24 14:20:50 +0000

    [chapter01]: all done

---
+++ src/exercise1b.java
@@ -0,0 +1,12 @@
+public class exercise1b {
+    public static void main(String[] args){
+        int x = 5;
+        while(x>1){
+            x--;
+
+            if(x<3){
+                System.out.println("small x");
+            }
+        }
+    }
+}
